8b179fac79d121ac1c227cf124d98535d0b8f92b
liujin <2454243839@qq.com>
2023-05-04 16:32:44 +0800

    vue2+webpack5

---
+++ src/views/my/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <div class="title">我的个人中心</div>
+    <button @click="$router.back()">返回</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+};
+</script>
+<style scoped lang="scss">
+.home {
+  color: rgb(255, 0, 157);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .title {
+    font-size: 30px;
+  }
+}
+</style>
