833a3a6700f96ec75788106e80c0cc069fef65e4
liujin <2454243839@qq.com>
2023-11-29 15:05:48 +0800

    测试按钮

--- src/views/components/child1.vue
+++ src/views/components/child1.vue
@@ -2,14 +2,24 @@
   <div class="child1">
     <div class="title">child1</div>
     <button @click="$bus.$emit('child1To2', 'params')">发出事件</button>
+    <br />
+    <button v-if="showBtn" @click="testClick()">发出事件002</button>
+    <button v-else>发出事件002</button>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      showBtn: true,
+    };
   },
   mounted() {},
+  methods: {
+    testClick() {
+      console.log("输出88888");
+    },
+  },
 };
 </script>
 <style scoped lang="scss">
